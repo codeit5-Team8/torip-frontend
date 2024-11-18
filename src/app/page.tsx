af00468731bd5b677eebbc15c592beb27e7db9a5
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-11-18 18:41:48 +0900

    test push main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
 export default function Home() {
-  const unusedVariable = "I'm not used"; // ESLint 경고 발생
-  console.log('This is a debug log'); // ESLint 경고 발생
-
   return <div className="bg-blue-500">Hello, Next.js!</div>;
 }
