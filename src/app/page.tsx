1fcabd9497786853830e299dcba53f25859ec491
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-11-18 17:45:54 +0900

    test eslint2

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 export default function Home() {
-  const unusedVariable = "I'm not used"; // no-unused-vars 경고
-  console.log('This is a debug log'); // no-console 경고
+  const unusedVariable = "I'm not used"; // ESLint 경고 발생
+  console.log('This is a debug log'); // ESLint 경고 발생
 
-  return <div className="bg-blue-500">codeIt</div>;
+  return <div className="bg-blue-500">Hello, Next.js!</div>;
 }
