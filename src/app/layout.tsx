694852cb52be4fd4b1bb847cab48f0cd62479380
Hayden <82023300+HaydenDevK@users.noreply.github.com>
2024-11-27 14:19:58 +0900

    [#3] Speed Insights 설치 (#11)
    * 🔧Chore: speed insights 패키지 설치
    
    * 🔧Chore: Next.js 버전 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,18 @@
 import type { Metadata } from 'next';
+import localFont from 'next/font/local';
 import './globals.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
+const geistSans = localFont({
+  src: './fonts/GeistVF.woff',
+  variable: '--font-geist-sans',
+  weight: '100 900',
+});
+const geistMono = localFont({
+  src: './fonts/GeistMonoVF.woff',
+  variable: '--font-geist-mono',
+  weight: '100 900',
+});
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -13,7 +26,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <head>
+        <title>Next.js</title>
+      </head>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   );
 }
