49e95e9dbf48c2143291eae035a932b987c4ae93
홍성숙 <134491629+SSUK-H@users.noreply.github.com>
2024-11-28 17:26:29 +0900

    [#22] Tailwind CSS 글로벌 스타일 설정 (#23)
    * 🔧Chore: Tailwind CSS 글로벌 스타일 설정
    
    - Pretendard 글로벌 폰트 적용
    - 컬러, 폰트 시스템 정의
    - 박스 그림자: shadow-base로 적용해주세요.
    - 중앙 정렬 유틸리티 클래스 정의: item-center로 적용해주세요.
    
    추후 디자인 컬러에 따라 수정 필요합니다. 공통 유틸리티 클래스가 있다면 자유롭게 추가해서 사용해주세요.
    
    * 🔧Chore: 반응형 Breakpoint 정의 추가
    
    반응형 모바일과 테블릿 Breakpoint 추가하였습니다.
    mobile:p-0 또는 tablet:m-0로 적용해주세요.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,45 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Pretendard', 'sans-serif'],
+      },
+      fontSize: {
+        xs: ['12px', '16px'],
+        sm: ['14px', '20px'],
+        base: ['16px', '24px'],
+        lg: ['18px', '28px'],
+        xl: ['20px', '28px'],
+        '2xl': ['24px', '32px'],
+        '3xl': ['30px', '36px'],
+      },
+      fontWeight: {
+        light: '300',
+        normal: '400',
+        medium: '500',
+        semibold: '600',
+        bold: '700',
+      },
       colors: {
-        background: 'var(--background)',
-        foreground: 'var(--foreground)',
+        primary: {
+          light: '#ffffff',
+          default: '#ffffff',
+          dark: '#ffffff',
+        },
+        secondary: {
+          light: '#000000',
+          default: '#000000',
+          dark: '#000000',
+        },
+        success: '#3B82F6',
+        warning: '#B91C1C',
+      },
+      boxShadow: {
+        base: '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
+      },
+      screens: {
+        mobile: '375px',
+        tablet: '744px',
       },
     },
   },
