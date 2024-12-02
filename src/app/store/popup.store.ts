07ac2d226dc0c70f36e49ab2345a45a3ec2166f8
JaeHyunGround <gothddlek@naver.com>
2024-12-02 14:58:17 +0900

    ♻️Refactor: popupText 타입 수정
    팝업에 들어갈 텍스트를 줄바꿈 기준으로 배열로 담아 보내는 형태가 아닌, 일반 문자열을 담을 수 있도록 편의성 향상을 위해 형태를 수정했습니다.

--- src/app/store/popup.store.ts
+++ src/app/store/popup.store.ts
@@ -2,13 +2,13 @@ import { create } from 'zustand';
 
 interface IPopupProps {
   isOpen: boolean; // 팝업 제어 boolean
-  popupText: string[]; // 팝업에 들어 갈 text 배열, 띄워쓰기 단위로 배열에 담아 보내면 보내시면 됩니다.
+  popupText: string; // 팝업에 들어 갈 text
   showCancelButton: boolean; // cancel 버튼 출력 유무 boolean
   confirmButtonText: string; // confirm 역할을 하는 버튼에 들어가는 text
   onConfirm?: () => void; // confirm 버튼 클릭 시 실행 할 함수
   onCancel?: () => void; // cancel 버튼 클릭 시 실행 할 함수
   showPopup: (
-    popupText: string[],
+    popupText: string,
     showCancelButton: boolean,
     confirmButtonText: string,
     onConfirm?: () => void,
@@ -19,7 +19,7 @@ interface IPopupProps {
 
 export const usePopupStore = create<IPopupProps>((set) => ({
   isOpen: false,
-  popupText: [],
+  popupText: '',
   showCancelButton: true,
   confirmButtonText: '확인',
   onConfirm: undefined,
@@ -42,7 +42,7 @@ export const usePopupStore = create<IPopupProps>((set) => ({
   closePopup: () =>
     set({
       isOpen: false,
-      popupText: [],
+      popupText: '',
       onConfirm: undefined,
       onCancel: undefined,
     }),
