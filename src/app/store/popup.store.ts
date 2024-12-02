a93e652fd7f510ba99c065bfce8cea11ee71b7da
JaeHyunGround <gothddlek@naver.com>
2024-12-02 14:22:07 +0900

    ✨Feat: Popup store 생성
    Popup 컴포넌트 제어를 위한 전역 스토어를 생성했습니다.

---
+++ src/app/store/popup.store.ts
@@ -0,0 +1,49 @@
+import { create } from 'zustand';
+
+interface IPopupProps {
+  isOpen: boolean;
+  popupText: string[];
+  showCancelButton: boolean;
+  confirmButtonText: string;
+  onConfirm?: () => void;
+  onCancel?: () => void;
+  showPopup: (
+    popupText: string[],
+    showCancelButton: boolean,
+    confirmButtonText: string,
+    onConfirm?: () => void,
+    onCancel?: () => void,
+  ) => void;
+  closePopup: () => void;
+}
+
+export const usePopupStore = create<IPopupProps>((set) => ({
+  isOpen: false,
+  popupText: [],
+  showCancelButton: true,
+  confirmButtonText: '확인',
+  onConfirm: undefined,
+  onCancel: undefined,
+  showPopup: (
+    popupText,
+    showCancelButton,
+    confirmButtonText,
+    onConfirm,
+    onCancel,
+  ) =>
+    set({
+      isOpen: true,
+      popupText,
+      showCancelButton,
+      confirmButtonText,
+      onConfirm,
+      onCancel,
+    }),
+  closePopup: () =>
+    set({
+      isOpen: false,
+      popupText: [],
+      onConfirm: undefined,
+      onCancel: undefined,
+    }),
+}));
