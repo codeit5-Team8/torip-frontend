c83351798c9b37f5a291525da9717ecccb75adab
JaeHyunGround <gothddlek@naver.com>
2024-12-02 14:30:34 +0900

    💡Comment: Popup store 주석 추가

--- src/app/store/popup.store.ts
+++ src/app/store/popup.store.ts
@@ -1,20 +1,20 @@
 import { create } from 'zustand';
 
 interface IPopupProps {
-  isOpen: boolean;
-  popupText: string[];
-  showCancelButton: boolean;
-  confirmButtonText: string;
-  onConfirm?: () => void;
-  onCancel?: () => void;
+  isOpen: boolean; // 팝업 제어 boolean
+  popupText: string[]; // 팝업에 들어 갈 text 배열, 띄워쓰기 단위로 배열에 담아 보내면 보내시면 됩니다.
+  showCancelButton: boolean; // cancel 버튼 출력 유무 boolean
+  confirmButtonText: string; // confirm 역할을 하는 버튼에 들어가는 text
+  onConfirm?: () => void; // confirm 버튼 클릭 시 실행 할 함수
+  onCancel?: () => void; // cancel 버튼 클릭 시 실행 할 함수
   showPopup: (
     popupText: string[],
     showCancelButton: boolean,
     confirmButtonText: string,
     onConfirm?: () => void,
     onCancel?: () => void,
-  ) => void;
-  closePopup: () => void;
+  ) => void; // 팝업 열기 함수, 팝업에 들어 갈 정보를 인자로 넘겨줍니다.
+  closePopup: () => void; // 팝업 닫기 함수
 }
 
 export const usePopupStore = create<IPopupProps>((set) => ({
