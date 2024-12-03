58d29716746c5ac653f5febc2cc8dc79b8f5c43a
suhyuk <101502480+kwonsuhyuk@users.noreply.github.com>
2024-12-03 18:40:35 +0900

    ✨Feat: 반응형 Header ui 및 NavTitle 생성 (#51)
    반응형으로 header 에 햄버거 아이콘 생성 및 글자크기 조정함
    navtitle에 각 페이지경로에 해당하는 페이지이름 지정할당

--- src/ui/common/Header.tsx
+++ src/ui/common/Header.tsx
@@ -1,3 +1,12 @@
+import NavTitle from './NavTitle';
+
 export default function Header() {
-  return <div>Header</div>;
+  return (
+    <div className="mb-4 flex items-center gap-4 bg-white px-4">
+      <button className="h-6 w-6 text-slate-400 tablet:hidden">
+        {/* 햄버거 아이콘 들어감, sidebar 여는 핸들러 등록필요함 */}=
+      </button>
+      <NavTitle />
+    </div>
+  );
 }
