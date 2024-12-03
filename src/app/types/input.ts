c42b6cf30bcee650959dd9fa2a5ee4ee44995a01
Hayden <82023300+HaydenDevK@users.noreply.github.com>
2024-12-03 13:42:58 +0900

    [#26] Input 공통 컴포넌트 제작 (#46)
    * 🔧Chore: shadcn init
    
    * 🔧Chore: tailwind 파일 경로 추가
    
    * 💄Design: shadcn Select 컴포넌트 커스텀
    
    * ✨Feat: Input 공통 컴포넌트 제작
    
    * ✨Feat: SelectInput 공통 컴포넌트 제작
    
    * ✨Feat: PasswordInput 제작
    
    * 🎨Style: 불필요한 twMerge 사용 코드 제거
    
    * ♻️Refactor: 타입 파일 분리
    
    * ♻️Refactor: globals.css 중복 구조 개선

---
+++ src/app/types/input.ts
@@ -0,0 +1,16 @@
+import { InputHTMLAttributes } from 'react';
+
+export type TInputErrorType =
+  | 'required'
+  | 'passwordTooShort'
+  | 'passwordsDoNotMatch'
+  | 'invalidEmail'
+  | 'passwordWrong'
+  | 'passwordNotSame';
+
+export interface IInputProps
+  extends Omit<InputHTMLAttributes<HTMLInputElement>, 'type'>,
+    React.RefAttributes<HTMLInputElement> {
+  type: 'text' | 'email';
+  errorType?: TInputErrorType;
+}
