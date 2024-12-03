d111eed3d782b99b68e9a11c8ab4f6ea3ad563f9
Hayden <82023300+HaydenDevK@users.noreply.github.com>
2024-12-03 13:42:58 +0900

    [#26] Input 공통 컴포넌트 제작 (#46)
    * 🔧Chore: shadcn init
    
    * 🔧Chore: tailwind 파일 경로 추가
    
    * 💄Design: shadcn Select 컴포넌트 커스텀
    
    * ✨Feat: Input 공통 컴포넌트 제작
    
    * ✨Feat: SelectInput 공통 컴포넌트 제작
    
    * ✨Feat: PasswordInput 제작
    
    * 🎨Style: 불필요한 twMerge 사용 코드 제거
    
    * ♻️Refactor: 타입 파일 분리
    
    * ♻️Refactor: globals.css 중복 구조 개선

---
+++ src/app/ui/common/Input.tsx
@@ -0,0 +1,38 @@
+import { forwardRef } from 'react';
+import { twMerge } from 'tailwind-merge';
+import { INPUT_CLASSNAME, INPUT_MESSAGE } from '@/constant/input';
+import { IInputProps } from '@/app/types/input';
+
+/**
+ * 공통 Input 컴포넌트 입니다.
+ *
+ * @param {'text' | 'email' } type - input 타입 입니다.
+ * @param {TErrorType | null} errorType - 해당하는 오류 메세지를 표시 합니다.선택 사항입니다.
+ * @param {string} label - input 필드에 대한 레이블 텍스트를 제공합니다. 선택 사항입니다.
+ * @returns {JSX.Element} - type에 맞는 입력 필드를 리턴합니다.
+ */
+
+const Input = forwardRef<HTMLInputElement, IInputProps>((props, ref) => {
+  const { errorType, ...rest } = props;
+
+  const inputClassName = twMerge(
+    INPUT_CLASSNAME.base,
+    errorType && INPUT_CLASSNAME.error.border,
+  );
+
+  return (
+    <div className="relative pb-7">
+      <input className={inputClassName} ref={ref} {...rest} />
+
+      {errorType && (
+        <p className={INPUT_CLASSNAME.error.message}>
+          {INPUT_MESSAGE.error[errorType]}
+        </p>
+      )}
+    </div>
+  );
+});
+
+Input.displayName = 'Input';
+
+export { Input };
