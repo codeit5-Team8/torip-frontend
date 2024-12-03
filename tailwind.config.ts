99cfad070a4eb2d5c0c79cae512f85c8e47b2d01
Hayden <hayden.dev.k@gmail.com>
2024-12-03 11:19:33 +0900

    🔧Chore: shadcn init

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from 'tailwindcss';
+import tailwindcssAnimate from 'tailwindcss-animate';
 
 export default {
+  darkMode: ['class'],
   content: [
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
@@ -9,22 +11,60 @@ export default {
   theme: {
     extend: {
       colors: {
-        primary: '#19D1CB',
-        secondary: '#000000',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
         mint: {
-          50: '#F3FCFC',
-          100: '#E6F9F9',
-          200: '#BFF3F3',
-          300: '#9AEEEF',
-          400: '#5BE1DD',
-          500: '#28D7D2',
-          600: '#19D1CB',
-          700: '#20ACAC',
-          800: '#1C8787',
-          900: '#156565',
-          950: '#115A5A',
+          '50': '#F3FCFC',
+          '100': '#E6F9F9',
+          '200': '#BFF3F3',
+          '300': '#9AEEEF',
+          '400': '#5BE1DD',
+          '500': '#28D7D2',
+          '600': '#19D1CB',
+          '700': '#20ACAC',
+          '800': '#1C8787',
+          '900': '#156565',
+          '950': '#115A5A',
         },
         warning: '#B91C1C',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        chart: {
+          '1': 'hsl(var(--chart-1))',
+          '2': 'hsl(var(--chart-2))',
+          '3': 'hsl(var(--chart-3))',
+          '4': 'hsl(var(--chart-4))',
+          '5': 'hsl(var(--chart-5))',
+        },
       },
       boxShadow: {
         base: '0 25px 50px -12px rgba(0, 0, 0, 0.25)',
@@ -33,7 +73,12 @@ export default {
         mobile: '375px',
         tablet: '744px',
       },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
     },
   },
-  plugins: [],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
