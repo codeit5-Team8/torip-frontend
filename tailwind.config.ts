41266a595217ddc059a1d1179eb4f76198b4506c
Hayden <hayden.dev.k@gmail.com>
2024-12-03 11:20:21 +0900

    🔧Chore: tailwind 파일 경로 추가

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,8 @@ export default {
     './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/constant/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
