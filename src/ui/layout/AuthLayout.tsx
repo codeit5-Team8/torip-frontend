3bbf2d3e817484b29555965c13e1c08b80ee43a4
Seongsuk Hong <ssukh77@naver.com>
2024-12-04 12:33:44 +0900

    ✨Feat: 로그인, 회원가입 레이아웃 및 로고 컴포넌트 구현
    - 로그인, 회원가입 레이아웃 잡았습니다.
    - 로고 컴포넌트 구현하였습니다.
      로고 컴포넌트는 아이콘 전용 및 로고 전체 사용과 로고 사이즈 옵션을 제공합니다.
      혹시 몰라서 스타일 수정할 수 있게 했습니다.
      그리고 로고 클릭 시 홈으로 이동하도록 구현했습니다.

---
+++ src/ui/layout/AuthLayout.tsx
@@ -0,0 +1,20 @@
+import Logo from '@ui/common/Logo';
+
+interface IAuthLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function AuthLayout({ children }: IAuthLayoutProps) {
+  return (
+    <div className="item-center">
+      <main className="container flex flex-col gap-10 pt-12 tablet:pt-16 desktop:pt-[7.5rem]">
+        <div className="item-center">
+          <Logo iconOnly={false} size="large" />
+        </div>
+
+        {/* Contents 영역 */}
+        {children}
+      </main>
+    </div>
+  );
+}
