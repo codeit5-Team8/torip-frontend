d271b9cbdc3a24ad0b44946fa51b87e6477136e1
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-12-05 09:47:40 +0900

    💄Design: 로그인 버튼 회원가입 폼완성시 색상변환
    isValid 속성을 이용해 회원가입 폼 인풋 채워질시
    primary색상으로 변하도록 구현

--- src/app/(unauthorized)/signup/page.tsx
+++ src/app/(unauthorized)/signup/page.tsx
@@ -18,90 +18,84 @@ export default function SignUpPage() {
     register,
     handleSubmit,
     watch,
-    formState: { errors },
-  } = useForm<ISignUpFormInputs>();
+    formState: { errors, isValid },
+  } = useForm<ISignUpFormInputs>({ mode: 'onChange' });
 
   const onSubmit: SubmitHandler<ISignUpFormInputs> = () => {
     // console.log('Form Data:', data);
   };
 
+  // passwordconfirm 이랑 비교하기위해 watch 사용
   const passwordValue = watch('password');
 
   return (
     <div>
       <form onSubmit={handleSubmit(onSubmit)} className="space-y-6">
         {/* Name */}
-        <div>
-          <AuthInput
-            type="name"
-            errorMessage={errors.name && errors.name.message}
-            {...register('name', {
-              required: '이름은 필수 항목입니다.',
-              pattern: {
-                value: AUTH_VALIDATION_REGEX.name,
-                message: '유효하지 않은 이름입니다.',
-              },
-              minLength: {
-                value: 2,
-                message: '이름은 최소 2자 이상이어야 합니다.',
-              },
-            })}
-          />
-        </div>
+        <AuthInput
+          type="name"
+          errorMessage={errors.name && errors.name.message}
+          {...register('name', {
+            required: '이름은 필수 항목입니다.',
+            pattern: {
+              value: AUTH_VALIDATION_REGEX.name,
+              message: '유효하지 않은 이름입니다.',
+            },
+            minLength: {
+              value: 2,
+              message: '이름은 최소 2자 이상이어야 합니다.',
+            },
+          })}
+        />
 
         {/* Email */}
-        <div>
-          <AuthInput
-            type="email"
-            errorMessage={errors.email && errors.email.message}
-            {...register('email', {
-              required: '이메일은 필수 항목입니다.',
-              pattern: {
-                value: AUTH_VALIDATION_REGEX.email,
-                message: '올바른 이메일 형식을 입력해 주세요.',
-              },
-              minLength: {
-                value: 2,
-                message: '이메일은 최소 2자 이상이어야 합니다.',
-              },
-            })}
-          />
-        </div>
-
+        <AuthInput
+          type="email"
+          errorMessage={errors.email && errors.email.message}
+          {...register('email', {
+            required: '이메일은 필수 항목입니다.',
+            pattern: {
+              value: AUTH_VALIDATION_REGEX.email,
+              message: '올바른 이메일 형식을 입력해 주세요.',
+            },
+            minLength: {
+              value: 2,
+              message: '이메일은 최소 2자 이상이어야 합니다.',
+            },
+          })}
+        />
         {/* Password */}
-        <div>
-          <AuthInput
-            type="password"
-            errorMessage={errors.password && errors.password.message}
-            {...register('password', {
-              required: '비밀번호는 필수 항목입니다.',
-              minLength: {
-                value: 8,
-                message: '비밀번호는 최소 8자 이상이어야 합니다.',
-              },
-            })}
-          />
-        </div>
-
+        <AuthInput
+          type="password"
+          errorMessage={errors.password && errors.password.message}
+          {...register('password', {
+            required: '비밀번호는 필수 항목입니다.',
+            minLength: {
+              value: 8,
+              message: '비밀번호는 최소 8자 이상이어야 합니다.',
+            },
+          })}
+        />
         {/* Passwordcheck */}
-        <div>
-          <AuthInput
-            type="passwordConfirm"
-            errorMessage={
-              errors.passwordConfirm && errors.passwordConfirm.message
-            }
-            {...register('passwordConfirm', {
-              required: '비밀번호 확인은 필수 항목입니다.',
-              validate: (value) =>
-                value === passwordValue || '비밀번호가 일치하지 않습니다.',
-            })}
-          />
-        </div>
-
+        <AuthInput
+          type="passwordConfirm"
+          errorMessage={
+            errors.passwordConfirm && errors.passwordConfirm.message
+          }
+          {...register('passwordConfirm', {
+            required: '비밀번호 확인은 필수 항목입니다.',
+            validate: (value) =>
+              value === passwordValue || '비밀번호가 일치하지 않습니다.',
+          })}
+        />
         {/* Submit Button */}
+        {/* 비활성화 일때 bg-slate-400 , 기본 bg-primary */}
         <Button
-          className="h-12 w-full bg-slate-400 text-base font-semibold"
+          className={`h-12 w-full border-none text-base font-semibold ${
+            isValid ? 'bg-primary' : 'bg-slate-400'
+          }`}
           type="submit"
+          disabled={!isValid}
         >
           회원가입하기
         </Button>
