ff4d3030d14af931993b0d1087c7e4c21687a3c7
Hayden <hayden.dev.k@gmail.com>
2024-12-12 11:38:51 +0900

    ✨Feat: NoteAllTripInfo 제작

---
+++ src/ui/note/NoteAddButton.tsx
@@ -0,0 +1,7 @@
+export default function NoteAddButton() {
+  return (
+    <button className="inline-flex h-[34px] w-[94px] items-center justify-center rounded-xl bg-primary text-white sm:h-12 sm:w-[180px]">
+      + 노트 작성
+    </button>
+  );
+}
