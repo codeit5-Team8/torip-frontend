077f04491368ae6bea7919aea5111f611aa13dac
Hayden <hayden.dev.k@gmail.com>
2024-12-12 13:41:39 +0900

    ✨Feat: 노트 작성 페이지로 이동 구현

--- src/ui/note/NoteAddButton.tsx
+++ src/ui/note/NoteAddButton.tsx
@@ -1,7 +1,14 @@
+import Link from 'next/link';
+
 export default function NoteAddButton() {
+  const sampleNoteId = '123';
+
   return (
-    <button className="inline-flex h-[34px] w-[94px] items-center justify-center rounded-xl bg-primary text-white sm:h-12 sm:w-[180px]">
-      + 노트 작성
-    </button>
+    <Link
+      href={`/note-add/${sampleNoteId}`}
+      className="inline-flex h-[34px] w-[94px] items-center justify-center rounded-xl bg-primary text-white sm:h-12 sm:w-[180px]"
+    >
+      + 노트 추가
+    </Link>
   );
 }
