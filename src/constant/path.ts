c2215bc1de2ae7cbabd216438218886932af6f8d
Hayden <hayden.dev.k@gmail.com>
2024-12-12 19:00:39 +0900

    ♻️Refactor: NavTitle 경로 체크 시 param 제외

--- src/constant/path.ts
+++ src/constant/path.ts
@@ -3,9 +3,9 @@ export const ROUTE_TITLE_MAP: Record<string, string> = {
   '/signin': '로그인',
   '/signup': '회원가입',
   '/todo-all': '모든 할일',
-  '/trip/:tripId': '여행 상세',
+  '/trip': '여행 상세',
   '/note-add': '노트 작성',
-  '/note-edit/:noteId': '노트 수정',
-  '/note-all-trip/:tripId': '여행 별 노트 모아보기',
-  '/note-all-task/:taskId': '할일 별 노트 모아보기',
+  '/note-edit': '노트 수정',
+  '/note-all-trip': '여행 별 노트 모아보기',
+  '/note-all-task': '할일 별 노트 모아보기',
 };
