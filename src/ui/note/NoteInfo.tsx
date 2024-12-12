0949ab26dbb31da21b009d2eef94d7bf2787a9ae
Hayden <hayden.dev.k@gmail.com>
2024-12-12 11:36:58 +0900

    ♻️Refactor: NoteTodoInfo 공통 컴포넌트화

--- src/ui/note/NoteInfo.tsx
+++ src/ui/note/NoteInfo.tsx
@@ -1,8 +1,10 @@
+import NoteTodoInfo from './NoteTodoInfo';
+
 export default function NoteInfo() {
   return (
     <section className="flex flex-col gap-3">
       <NoteTripInfo />
-      <NoteTodoInfo />
+      <NoteTodoInfo title="할 일 제목" />
     </section>
   );
 }
@@ -15,19 +17,6 @@ function NoteTripInfo() {
       <p className="text-base font-medium leading-normal text-slate-800">
         여행 제목
       </p>
-    </div>
-  );
-}
-
-function NoteTodoInfo() {
-  return (
-    <div className="flex items-center gap-[6px]">
-      <span className="inline-flex h-5 items-center justify-center rounded bg-slate-100 px-[3px] py-0.5 text-xs font-medium leading-none text-slate-700">
-        To do
-      </span>
-      <span className="text-sm font-normal leading-tight text-slate-700">
-        할 일 제목
-      </span>
     </div>
   );
 }
