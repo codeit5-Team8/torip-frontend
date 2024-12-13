1fd56d0ccacba7ca4ddd25429fa6e49abe511c49
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-12-13 10:50:21 +0900

    🚚Rename: EveryTodoBox -> AllTodoBox 로 이름 변경
    EveryTodoBox -> AllTodoBox 로 이름 변경 및
    모든 할일버튼에 router 기능 등록

--- src/app/(authorized)/page.tsx
+++ src/app/(authorized)/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import EveryTodoBox from '@ui/box/EveryTodoBox';
+import AllTodoBox from '@ui/box/AllTodoBox';
 import MyProgressBox from '@ui/box/MyProgrssBox';
 import TripBox from '@ui/box/TripBox';
 import NavTitle from '@ui/common/NavTitle';
@@ -11,12 +11,13 @@ export default function Home() {
       <NavTitle />
       <section className="grid w-full grid-cols-1 gap-2 pt-4 tablet:grid-cols-2 tablet:gap-6">
         {/* task 전체 불러오기(?) 필요 */}
-        <EveryTodoBox />
+        <AllTodoBox />
         {/* progress 퍼센테이지 불러오는 것 필요 */}
         <MyProgressBox />
       </section>
       {/* 각 여행 별 할일 컨테이너 */}
       {/* 유저가 포함되어있는 여행들 불러오는 것 필요 */}
+      {/* 여행 없을시에 보여주는 로직 필요 -> api 연결후에  */}
       {/* 여행별 taskdata 필요 */}
       <section className="mt-6 flex flex-col gap-6">
         <article>
--- src/ui/box/AllTodoBox.tsx
+++ src/ui/box/AllTodoBox.tsx
@@ -1,9 +1,15 @@
+'use client';
 import Button from '@ui/common/Button';
 import Subtitle from '@ui/common/Subtitle';
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 
 //task를 어떻게 어디서 불러오느냐에 따라 props 변경하기
-export default function EveryTodoBox() {
+export default function AllTodoBox() {
+  const navigate = useRouter();
+  const navTodoAll = () => {
+    navigate.push('/todo-all');
+  };
   return (
     <div className="h-[250px] rounded-xl bg-white px-4 pb-6 pt-4 tablet:px-6">
       <div className="flex items-center justify-between">
@@ -11,9 +17,12 @@ export default function EveryTodoBox() {
           title="모든 할 일"
           icon="everytodo"
           iconBg="bg-primary"
-          link="/signup"
+          link="/todo-all"
         />
-        <Button className="flex items-center whitespace-nowrap border-none bg-white text-sm font-medium text-slate-600 hover:bg-slate-100 active:bg-slate-100">
+        <Button
+          className="flex items-center whitespace-nowrap border-none bg-white text-sm font-medium text-slate-600 hover:bg-slate-100 active:bg-slate-100"
+          onClick={navTodoAll}
+        >
           <span className="ml-2">전체 보기</span>
           <Image
             src={`/asset/icon/arrowright.png`}
