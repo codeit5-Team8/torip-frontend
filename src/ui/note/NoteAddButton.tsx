2e5969679ff8f58ce32a9ff55769848dd381cfcb
Hayden <hayden.dev.k@gmail.com>
2024-12-13 09:16:10 +0900

    ✨Feat: 노트 추가 버튼으로 이동 시 query로 tripId, taskId 전달
    - as로 사용자에게 query 노출되지 않게 마스킹

--- src/ui/note/NoteAddButton.tsx
+++ src/ui/note/NoteAddButton.tsx
@@ -1,11 +1,16 @@
 import Link from 'next/link';
 
 export default function NoteAddButton() {
-  const sampleNoteId = '123';
+  const sampleTripId = '123';
+  const sampleTaskId = '123';
 
   return (
     <Link
-      href={`/note-add/${sampleNoteId}`}
+      href={{
+        pathname: '/note-add',
+        query: { tripId: sampleTripId, taskId: sampleTaskId },
+      }}
+      as="/note-add"
       className="inline-flex h-[34px] w-[94px] items-center justify-center rounded-xl bg-primary text-white sm:h-12 sm:w-[180px]"
     >
       + 노트 추가
