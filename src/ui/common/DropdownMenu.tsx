409e313f97f36d83228e7fb7b13ca2069fec054a
홍성숙 <134491629+SSUK-H@users.noreply.github.com>
2024-12-14 19:38:20 +0900

    [#76] DropdownMenu trigger 버튼 아이콘이나 텍스트, 스타일 정의 추가 (#82)
    * ✨Feat: DropdownMenu trigger 버튼 아이콘이나 텍스트, 스타일 정의 추가
    
    DropdownMenu trigger 버튼 모양이 다른 경우가 있어
    버튼 아이콘이나 텍스트, 스타일 props로 내려 받도록 수정하였습니다.

--- src/ui/common/DropdownMenu.tsx
+++ src/ui/common/DropdownMenu.tsx
@@ -4,6 +4,8 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from '@components/ui/dropdown-menu';
+import { ReactNode } from 'react';
+import { twMerge } from 'tailwind-merge';
 
 interface IDropdownMenuProps {
   items: {
@@ -11,14 +13,26 @@ interface IDropdownMenuProps {
     onClick: () => void; // 항목을 클릭했을 때 실행되는 함수
     disabled?: boolean; // 항목이 비활성화 상태인지 여부
   }[];
+  className?: string; // 버튼 스타일
+  children: ReactNode; // 트리거 버튼 텍스트나 아이콘
 }
 
-export default function DropdownMenu({ items }: IDropdownMenuProps) {
+export default function DropdownMenu({
+  items,
+  className,
+  children,
+}: IDropdownMenuProps) {
   return (
     <DropDownMenuWrapper>
       <DropdownMenuTrigger asChild>
-        <button className="relative flex h-6 w-6 items-center justify-center rounded-full bg-slate-50 text-slate-400">
-          {/* 케밥 아이콘 부분 */}
+        <button
+          className={twMerge(
+            'relative flex min-h-6 min-w-6 items-center justify-center rounded-full bg-white text-slate-400',
+            className,
+          )}
+        >
+          {/* 아이콘이나 텍스트 */}
+          {children}
         </button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
