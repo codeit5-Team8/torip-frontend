de0b4b439060f42d95e8462c80fe27038051e43b
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-12-16 17:09:33 +0900

    [#83] SideBar 컴포넌트 구조 수정 (#93)
    * 🚚Rename: SideBar, SideBarContent 컴포넌트 파일 위치 변경
    
    * ♻️Refactor: 파일 경로가 변경됨에 따라 import문 수정
    
    * ♻️Refactor: SideBarContent 컴포넌트 내용 구조 분해

---
+++ src/ui/SideBar/SideBarContent.tsx
@@ -0,0 +1,68 @@
+import Image from 'next/image';
+import Button from '../common/Button';
+import TodoModal from '../common/TodoModal';
+import TripModal from '../common/TripModal';
+import { useModalStore } from '@store/modal.store';
+import UserInfo from '@ui/SideBar/UserInfo';
+import Dashboard from '@ui/SideBar/Dashboard';
+import TripList from '@ui/SideBar/TripList';
+
+interface ISideBarContentProps {
+  clickSideBarContent: () => void;
+}
+export default function SideBarContent({
+  clickSideBarContent,
+}: ISideBarContentProps) {
+  const { showModal } = useModalStore();
+
+  const handleAddTodo = () => {
+    showModal({
+      title: '할 일 생성',
+      content: <TodoModal />,
+    });
+  };
+
+  const handleAddTrip = () => {
+    showModal({
+      title: '여행 생성',
+      content: <TripModal />,
+    });
+  };
+
+  return (
+    <div className="w-full">
+      <section className="flex flex-col gap-6 border-b-[1px] border-slate-200 px-6 pb-6">
+        <UserInfo />
+        <Button
+          className="h-12 w-full gap-0.5 p-3 text-base font-semibold"
+          onClick={handleAddTodo}
+        >
+          <Image
+            src="/asset/icon/plus.png"
+            alt="할 일 추가 버튼"
+            width={24}
+            height={24}
+          />
+          새 할 일
+        </Button>
+      </section>
+      <Dashboard clickSideBarContent={clickSideBarContent} />
+      <section className="p-6">
+        <TripList clickSideBarContent={clickSideBarContent} />
+        <Button
+          variant="outlined"
+          className="h-12 w-full gap-0.5 p-3 text-base font-semibold"
+          onClick={handleAddTrip}
+        >
+          <Image
+            src="/asset/icon/plus-mint.png"
+            alt="여행 추가 버튼"
+            width={24}
+            height={24}
+          />
+          새 여행
+        </Button>
+      </section>
+    </div>
+  );
+}
