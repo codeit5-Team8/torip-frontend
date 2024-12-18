bde5e08e51ddcdd53a9e6e6a745c4320f99b93b9
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2024-12-18 11:05:44 +0900

    ✨Feat: 여행 목록 api 연동 및 모든할일 api 훅
    여행 목록 api 연동 및 모든할일 api 훅작성
    디자인 추가수정 및 모든할일 api 가져오는 부분 수정 필요

--- src/constant/queryKeyFactory.ts
+++ src/constant/queryKeyFactory.ts
@@ -1,6 +1,8 @@
 import { getTask, getTaskProgress } from '@lib/api/service/task.api';
+import { getTripList } from '@lib/api/service/trip.api';
 import { createQueryKeys } from '@lukemorales/query-key-factory';
 import { TGetTaskRequest } from '@model/task.model';
+import { TGetTripListProps } from '@model/trip.model';
 
 export const tasksQueryKeys = createQueryKeys('tasks', {
   list: (props: TGetTaskRequest) => ({
@@ -14,5 +16,12 @@ export const progressQueryKeys = createQueryKeys('progress', {
   progress: () => ({
     queryKey: ['taskProgress'],
     queryFn: () => getTaskProgress(),
+  }),
+});
+
+export const tripQueryKeys = createQueryKeys('trip', {
+  list: (query: TGetTripListProps) => ({
+    queryKey: ['tripList'],
+    queryFn: () => getTripList(query),
   }),
 });
