befb6dddcf949d223ee54476b4eabf1c1373cb49
Seongsuk Hong <ssukh77@naver.com>
2024-12-24 09:50:21 +0900

    🐛Fix: 이미지 엑박 문제 해결
    .png로 끝나는 경로에 대해 미들웨어가 호출되지 않도록 설정 추가

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,5 +13,7 @@ export default async function middleware(req: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|signin|signup).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|.*\\.png$|favicon.ico|signin|signup).*)',
+  ],
 };
