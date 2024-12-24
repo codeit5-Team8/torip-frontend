5db02a2d2142932c6e54702e3779638a35937339
Seongsuk Hong <ssukh77@naver.com>
2024-12-24 09:39:40 +0900

    ✨Feat: 비로그인 상태에서 특정 경로 요청 시 로그인 리다이렉트 middleware 설정
    - Next.js middleware 설정
    - 정적/이미지 파일, API, 로그인, 회원가입 경로를 제외한 모든 요청 경로 처리
    - 비로그인 상태에서 해당 경로 요청 시 로그인 페이지로 리다이렉트

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,4 +1,17 @@
-// middle ware 필요한 것 작성
-export default async function middleware() {
-  return;
+import { getToken } from 'next-auth/jwt';
+import { NextRequest, NextResponse } from 'next/server';
+
+export default async function middleware(req: NextRequest) {
+  const secret = process.env.NEXTAUTH_SECRET;
+  const token = await getToken({ req, secret });
+
+  if (!token) {
+    return NextResponse.redirect(new URL('/signin', req.url));
+  }
+
+  return NextResponse.next();
 }
+
+export const config = {
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|signin|signup).*)'],
+};
