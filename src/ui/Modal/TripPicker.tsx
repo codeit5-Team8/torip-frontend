f8ede53dbe47447f81b630244efa4b8429a55df8
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2025-01-01 04:20:10 +0900

    ✨Feat: 여행 선택 컴포넌트 구현

---
+++ src/ui/Modal/TripPicker.tsx
@@ -0,0 +1,36 @@
+import { useGetUserTripList } from '@hooks/trip/useGetUserTripList';
+import SelectInput from '@ui/common/SelectInput';
+
+interface ITripPicker {
+  tripId: number;
+  setTripId: React.Dispatch<React.SetStateAction<number>>;
+}
+
+export default function TripPicker({ tripId = 33, setTripId }: ITripPicker) {
+  const { data: tripList, isLoading } = useGetUserTripList({
+    lastSeenId: 0,
+  });
+
+  return (
+    <>
+      {!isLoading && (
+        <>
+          <header className="my-4 text-base font-semibold text-slate-800">
+            여행 선택
+          </header>
+          <SelectInput
+            options={tripList!.content.map((trip) => {
+              return { label: trip.name, value: `${trip.id}` };
+            })}
+            onChange={(newValue) => {
+              setTripId(Number(newValue));
+            }}
+            value={tripId}
+            defaultTrip={tripId}
+            placeholder="여행을 선택해주세요."
+          />
+        </>
+      )}
+    </>
+  );
+}
