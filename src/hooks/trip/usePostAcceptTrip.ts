d016392f8714bb80e22ebd864ef66ffef735e7a1
Seongsuk Hong <ssukh77@naver.com>
2024-12-31 17:39:22 +0900

    ✨Feat: 여행 승인/거절 리팩토링
    - 여행 승인/거절 ID 값 넘겨서 처리하도록 수정
    - 여행 거절 API url 오타 수정
    - 호출 성공 시 여행 id 반환 받아 여행 id 넘겨 참여 리스트 재호출 처리
    (백엔드 api 수정 요청 상태.. 확인 필요..)

--- src/hooks/trip/usePostAcceptTrip.ts
+++ src/hooks/trip/usePostAcceptTrip.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { tripQueryKeys } from '@constant/queryKeyFactory';
 import { postAcceptTrip } from '@lib/api/service/trip.api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
@@ -6,13 +7,19 @@ export const usePostAcceptTrip = () => {
   const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async (id: number) => {
-      await postAcceptTrip(id);
-      return id;
+      const response = await postAcceptTrip(id);
+      return response;
     },
-    onSuccess: (id: number) => {
-      queryClient.invalidateQueries({
-        queryKey: tripQueryKeys.joinList(id).queryKey,
-      });
+    onSuccess: (response) => {
+      // TODO: api 수정 후 여행 id 넘겨 참여 리스트 재호출 처리 확인 필요
+      if (response.success) {
+        // queryClient.invalidateQueries(
+        //   {
+        //     queryKey: tripQueryKeys.joinList(response..result.id).queryKey,
+        //   },
+        //   { throwOnError: true },
+        // );
+      }
     },
     onError: (error) => {
       // eslint-disable-next-line no-console
--- src/hooks/trip/usePostRejectTrip.ts
+++ src/hooks/trip/usePostRejectTrip.ts
@@ -1,18 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { tripQueryKeys } from '@constant/queryKeyFactory';
 import { postRejectTrip } from '@lib/api/service/trip.api';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 
 export const usePostRejectTrip = () => {
-  const queryClient = useQueryClient();
+  // const queryClient = useQueryClient();
   const mutation = useMutation({
     mutationFn: async (id: number) => {
-      await postRejectTrip(id);
-      return id;
+      const response = await postRejectTrip(id);
+      return response;
     },
-    onSuccess: (id: number) => {
-      queryClient.invalidateQueries({
-        queryKey: tripQueryKeys.joinList(id).queryKey,
-      });
+    onSuccess: (response) => {
+      // TODO: api 수정 후 여행 id 넘겨 참여 리스트 재호출 처리 확인 필요
+      if (response.success) {
+        // queryClient.invalidateQueries({
+        //   queryKey: ['trip', 'joinList', 'member'],
+        // });
+        // queryClient.invalidateQueries(
+        //   {
+        //     queryKey: tripQueryKeys.joinList(response..result.id).queryKey,
+        //   },
+        //   { throwOnError: true },
+        // );
+      }
     },
     onError: (error) => {
       // eslint-disable-next-line no-console
