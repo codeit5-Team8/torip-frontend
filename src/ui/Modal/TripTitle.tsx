514b57ef1a6b5688b7415741c26af8d5f7f280fb
Jaehyun Ahn <97944429+JaeHyunGround@users.noreply.github.com>
2024-12-31 20:38:26 +0900

    ♻️Refactor: placeholder 추가

--- src/ui/Modal/TripTitle.tsx
+++ src/ui/Modal/TripTitle.tsx
@@ -14,7 +14,12 @@ export default function TripTitle({ tripName, setTripName }: ITripTitle) {
       <header className="mb-2 text-base font-semibold text-slate-800">
         여행 제목
       </header>
-      <Input type="text" value={tripName} onChange={handleTripName} />
+      <Input
+        type="text"
+        value={tripName}
+        onChange={handleTripName}
+        placeholder="여행의 제목을 적어주세요"
+      />
     </div>
   );
 }
