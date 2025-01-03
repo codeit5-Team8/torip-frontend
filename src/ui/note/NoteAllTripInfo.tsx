7ef742e06fd35c3346a0950aac331a4eb0145c53
kwonsuhyuk <tngur0716@hanyang.ac.kr>
2025-01-03 10:10:04 +0900

    ✨Feat: 할일 별 노트 페이지 (아이콘 및 response 수정필요)
    할일 별 노트 페이지 (아이콘 및 response 수정필요)

--- src/ui/note/NoteAllTripInfo.tsx
+++ src/ui/note/NoteAllTripInfo.tsx
@@ -1,21 +1,36 @@
 import NoteAddButton from '@ui/note/NoteAddButton';
+import Image from 'next/image';
+import NoteTaskInfo from './NoteTaskInfo';
 
 interface INoteAllTripInfoProps {
-  tripId: number;
+  tripId?: number;
+  taskId?: number;
   tripTitle: string;
+  taskTitle?: string;
 }
 
 export default function NoteAllTripInfo({
   tripId,
+  taskId,
+  taskTitle,
   tripTitle,
 }: INoteAllTripInfoProps) {
+  // tripid , taskid 선택적으로 받아옴
+  const idProps = tripId ? { tripId } : { taskId };
+
   return (
     <section className="mb-4 flex h-[52px] items-center gap-2 rounded-xl bg-white px-[11px] py-[9px] sm:h-[88px] sm:px-6 sm:py-5">
-      <>아이콘</>
-      <div className="flex-1 text-sm font-semibold leading-tight text-slate-800">
+      <Image
+        src="/asset/icon/trip-dark.png"
+        alt="여행아이콘"
+        width={24}
+        height={24}
+      />
+      <div className="flex-1 flex-col justify-center text-sm font-semibold leading-tight text-slate-800">
         {tripTitle}
+        {taskId && taskTitle && <NoteTaskInfo taskTitle={taskTitle} />}
       </div>
-      <NoteAddButton tripId={tripId} />
+      <NoteAddButton {...idProps} />
     </section>
   );
 }
--- src/ui/note/NoteList.tsx
+++ src/ui/note/NoteList.tsx
@@ -3,14 +3,17 @@ import NoteItem from './NoteItem';
 
 interface INoteListProps {
   notes: TNote[];
-  tripId: number;
+  tripId?: number;
+  taskId?: number;
 }
 
-export default function NoteList({ notes, tripId }: INoteListProps) {
+export default function NoteList({ notes, tripId, taskId }: INoteListProps) {
+  // tripid , taskid 선택적으로 받아옴
+  const idProps = tripId ? { tripId } : { taskId };
   return (
     <>
       {notes.map((note) => (
-        <NoteItem key={note.noteId} note={note} tripId={tripId} />
+        <NoteItem key={note.noteId} note={note} {...idProps} />
       ))}
     </>
   );
